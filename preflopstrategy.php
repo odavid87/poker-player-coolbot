a8fa968c8c56303a48ab5beeb553e9e00efc3469
david.orban <david.orban@emarsys.com>
2015-03-21 15:04:59 +0100

    modified preflop strat

--- preflopstrategy.php
+++ preflopstrategy.php
@@ -4,9 +4,7 @@ class PreFlopStrategy extends AbstractBetStrategy
     public function betRequest(Game $game)
     {
         $myself = $game->getActivePlayer();
-        if ($game->isDealer($myself)) {
-            return $this->betAmount($game->minimalBid());
-        } else if ($myself->getMyHand()->hasPotential()) {
+        if ($myself->getMyHand()->hasPotential()) {
             return $this->betAmount($game->minimalBid());
         } else {
             return 0;
