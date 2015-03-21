01a4b7421be4a12c6253d71a7d932a139064d6ab
cavalierrant <cavalierrant@gmail.com>
2015-03-21 13:55:15 +0000

    straighttest

---
+++ valuevalidator/straight.php
@@ -0,0 +1,21 @@
+<?php
+class StraightValueValidator extends AbstractValueValidator
+{
+    public function isValid(array $cards)
+    {
+        $values = array_keys($this->ranks);
+        sort($values);
+        
+        $total = 0;
+        $prev = -1;
+        foreach ($values as $value) {
+            if ($prev + 1 == $value)
+            {
+                $total++;
+            }
+            $prev = $value;
+        }
+        
+        return $total >= 5;
+    }
+}
