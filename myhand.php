83792371e2fd1140bdbe35f4f1dd75ec2f033053
david.orban <david.orban@emarsys.com>
2015-03-21 10:12:39 +0100

    myhand created

---
+++ myhand.php
@@ -0,0 +1,16 @@
+<?php
+class myHand
+{
+    /**
+     * @var array
+     */
+    private $cards;
+
+    public function __construct(array $cards)
+    {
+
+        $this->cards = $cards;
+    }
+
+
+}
